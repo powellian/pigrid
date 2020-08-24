b7cc4ef52cf740838f29ffb965997a17df01fc9a
powellian <ianpowell01@gmail.com>
2020-08-24 20:50:34 +0100

    code comment cleanup etc

--- src/scripts/pi-modules/gradient-block.js
+++ src/scripts/pi-modules/gradient-block.js
@@ -1,40 +1,6 @@
-/* eslint-disable no-undef */
-console.log('IN: Grad Loops');
-
-// export default () => {
-// 	let gradElements = document.querySelectorAll('.gradient-block .pigr__pod');
-// 	for (let i = 0; i < gradElements.length; i++) {
-// 		console.log(`gradElements: ${gradElements}`);
-// 		console.log(`gradElements classes: ${gradElements.classlist}`);
-// 		gradElements[i].classList.add(`gradient-block__inner--0${i}`);
-// 	}
-// }
-// export default;
-
-// THIS SHOULD WORK - returns Object error
 export default () => {
-    // export function theGradients(theClassName) {
-    // const theGradients = (theClassName) => {
-    let gradElements = document.querySelectorAll('.gradient-block .pigr__pod');
-    for (let i = 0; i < gradElements.length; i++) {
-        console.log(`gradElements: ${gradElements}`);
-        console.log(`gradElements classes: ${gradElements.classlist}`);
-        gradElements[i].classList.add(`gradient-block__inner--0${i}`);
-    }
+  let gradElements = document.querySelectorAll('.gradient-block .pigr__pod');
+  for (let i = 0; i < gradElements.length; i++) {
+    gradElements[i].classList.add(`gradient-block__inner--0${i}`);
+  }
 };
-// export {
-// 	theGradients as
-// 	default
-// };
-
-/** THIS WORKS IN ISOLATION */
-// let gradElements = document.querySelectorAll('.gradient-block .pigr__pod');
-// for (let i = 0; i < gradElements.length; i++) {
-// 	console.log(`gradElements: ${gradElements}`);
-// 	console.log(`gradElements classes: ${gradElements.classlist}`);
-// 	gradElements[i].classList.add(`gradient-block__inner--0${i}`);
-// }
-// export {
-// 	theGradElements as
-// 	default
-// };
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,3 @@
-// console.log('%c Hello from /scripts/script.js ', 'background: lemonchiffon; border: 1px solid #fff');
-// console.log('%c Welcome to tris-webpack-boilerplate! ', 'background: lemonchiffon; border: 1px solid #fff');
-// console.log('%c Coded by: https://tris.codes 💠', 'background: lavenderblush; border: 1px solid #000; padding: 4px; padding-top: 10px; padding-bottom: 8px;');
-
-// add custom js below
-
-// import * as addContent from './pi-modules/add-content-click';
-// import * as squareRoot from './pi-modules/sq-root'
-// console.log(`sq-root.js: ${squareRoot.square(350)}`); // 122500
-// console.log(`sq-root.js: ${squareRoot.diag(27, 58).toFixed(3)}`); // 63.XYZ
-// squareRoot.default;
+console.log('%c Welcome to tris-webpack-boilerplate! ', 'background: lemonchiffon; border: 1px solid #fff');
 
 import './pi-modules/index'
